249525c4776c39be4abdae0b32d69b0da7dbd8bb
Rikhil <23627977+rikhilrai@users.noreply.github.com>
2022-02-02 11:33:06 +0000

    .

--- src/case_insensitive_dict/case_insensitive_dict.py
+++ src/case_insensitive_dict/case_insensitive_dict.py
@@ -12,7 +12,6 @@ from typing import Optional
 from typing import Tuple
 from typing import TypeVar
 
-T = TypeVar('T')  # pylint: disable=invalid-name
 KT = TypeVar('KT')  # pylint: disable=invalid-name
 VT = TypeVar('VT')  # pylint: disable=invalid-name
 
